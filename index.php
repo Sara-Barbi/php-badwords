603444fa066948c2bee927390d1590f869b4a05d
Sara-Barbi <barbisara02@gmail.com>
2022-02-14 17:35:01 +0100

    lunghezza

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
-  $paragrafoTesto="ambaraba-cci-ccoccò tre civette sul comò che facevano l'amore con la figlia del dottore";
-  $lunghezzaParagrafo=strlen($paragrafoTesto);
+  $paragrafoTesto="ambaraba-cci-ccoccò tre civette sul comò che facevano l'amore con la figlia del dottore op";
+  $arrayCreato=explode(" ",$paragrafoTesto);
+
+
+  $lunghezzaParagrafo = count($arrayCreato);
+  $lunghezzaStringa = strlen($paragrafoTesto);
 ?>
 
 
 
+<div>
+  <h3> <?php echo $paragrafoTesto; ?></h3>
+  <p>lunghezza filastrocca : <?php echo $lunghezzaStringa ?> lettere e spazi</p>
+  <p>lunghezza filastrocca : <?php echo $lunghezzaParagrafo ?> parole</p>
 
-<h3> <?php echo $paragrafoTesto; ?></h3>
-<p>lunghezza filastrocca : <?php $lunghezzaParagrafo?> </p>
+
+</div>
