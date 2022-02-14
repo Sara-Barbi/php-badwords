2caf8d2f00bc3ea663532d6fb370cfe73590d4d2
Sara-Barbi <barbisara02@gmail.com>
2022-02-14 17:24:11 +0100

    paragrafo

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+  $paragrafoTesto="ambaraba-cci-ccoccò tre civette sul comò che facevano l'amore con la figlia del dottore";
+  $lunghezzaParagrafo=strlen($paragrafoTesto);
+?>
+
+
+
+
+<h3> <?php echo $paragrafoTesto; ?></h3>
+<p>lunghezza filastrocca : <?php $lunghezzaParagrafo?> </p>
